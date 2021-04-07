b694855fa1fd6c188de8f0578be96fec9679751a
liuheyong <17682347237@163.com>
2021-04-07 18:20:03 +0800

    fix

--- src/main/java/com/agent/boot/lhy/agent/DeleteService.java
+++ src/main/java/com/agent/boot/lhy/agent/DeleteService.java
@@ -1,18 +1,18 @@
-//package com.agent.boot.lhy.agent;
-//
-//import java.util.List;
-//
-///**
-// * @author: liuheyng
-// * @date: 2021/4/6 22:48
-// * @description: DeleteService
-// */
-//public class DeleteService {
-//
-//    public void delete(List<Integer> list) {
-//        for (int i = 0; i < list.size(); i++) {
-//            list.remove(i);
-//        }
-//    }
-//
-//}
+package com.agent.boot.lhy.agent;
+
+import java.util.List;
+
+/**
+ * @author: liuheyng
+ * @date: 2021/4/6 22:48
+ * @description: DeleteService
+ */
+public class DeleteService {
+
+    public void delete(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.remove(i);
+        }
+    }
+
+}
--- src/main/java/com/agent/boot/lhy/agent/service/ServiceTest.java
+++ src/main/java/com/agent/boot/lhy/agent/service/ServiceTest.java
@@ -1,35 +1,35 @@
-//package com.agent.boot.lhy.agent.service;
-//
-//import com.agent.boot.lhy.agent.DeleteService;
-//import com.agent.boot.lhy.agent.InsertService;
-//
-//import java.util.LinkedList;
-//import java.util.List;
-//
-///**
-// * @author: liuheyng
-// * @date: 2021/4/6 23:46
-// * @description: ServiceTest
-// */
-//public class ServiceTest {
-//
-//    public static void main(String[] args) {
-//        //插入服务
-//        InsertService insertService = new InsertService();
-//        //删除服务
-//        DeleteService deleteService = new DeleteService();
-//        System.out.println("....begin insert....");
-//        insertService.insert1(1040);
-//        insertService.insert2(20000);
-//        insertService.insert3(30203);
-//
-//        System.out.println(".....end insert.....");
-//        List<Integer> list = new LinkedList<>();
-//        for (int i = 0; i < 29440; i++) {
-//            list.add(i);
-//        }
-//        System.out.println(".....begin delete......");
-//        deleteService.delete(list);
-//        System.out.println("......end delete........");
-//    }
-//}
+package com.agent.boot.lhy.agent.service;
+
+import com.agent.boot.lhy.agent.DeleteService;
+import com.agent.boot.lhy.agent.InsertService;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: liuheyng
+ * @date: 2021/4/6 23:46
+ * @description: ServiceTest
+ */
+public class ServiceTest {
+
+    public static void main(String[] args) {
+        //插入服务
+        InsertService insertService = new InsertService();
+        //删除服务
+        DeleteService deleteService = new DeleteService();
+        System.out.println("....begin insert....");
+        insertService.insert1(1040);
+        insertService.insert2(20000);
+        insertService.insert3(30203);
+
+        System.out.println(".....end insert.....");
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 29440; i++) {
+            list.add(i);
+        }
+        System.out.println(".....begin delete......");
+        deleteService.delete(list);
+        System.out.println("......end delete........");
+    }
+}
