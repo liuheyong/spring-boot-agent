4058accc68733739d01e887d4e9d7acf36126100
liuheyong <17682347237@163.com>
2021-04-07 17:33:35 +0800

    修改依赖

--- src/main/java/com/agent/boot/lhy/agent/InsertService.java
+++ src/main/java/com/agent/boot/lhy/agent/InsertService.java
@@ -1,39 +1,39 @@
-package com.agent.boot.lhy.agent;
-
-import java.util.LinkedList;
-import java.util.List;
-
-/**
- * @author: liuheyng
- * @date: 2021/4/6 22:46
- * @description: InsertService
- */
-public class InsertService {
-
-    /**
-     * 模拟数据插入
-     *
-     * @param num
-     */
-    public void insert2(int num) {
-        List<Integer> list = new LinkedList<>();
-        for (int i = 0; i < num; i++) {
-            list.add(i);
-        }
-    }
-
-    public void insert1(int num) {
-        List<Integer> list = new LinkedList<>();
-        for (int i = 0; i < num; i++) {
-            list.add(i);
-        }
-    }
-
-    public void insert3(int num) {
-        List<Integer> list = new LinkedList<>();
-        for (int i = 0; i < num; i++) {
-            list.add(i);
-        }
-    }
-
-}
+//package com.agent.boot.lhy.agent;
+//
+//import java.util.LinkedList;
+//import java.util.List;
+//
+///**
+// * @author: liuheyng
+// * @date: 2021/4/6 22:46
+// * @description: InsertService
+// */
+//public class InsertService {
+//
+//    /**
+//     * 模拟数据插入
+//     *
+//     * @param num
+//     */
+//    public void insert2(int num) {
+//        List<Integer> list = new LinkedList<>();
+//        for (int i = 0; i < num; i++) {
+//            list.add(i);
+//        }
+//    }
+//
+//    public void insert1(int num) {
+//        List<Integer> list = new LinkedList<>();
+//        for (int i = 0; i < num; i++) {
+//            list.add(i);
+//        }
+//    }
+//
+//    public void insert3(int num) {
+//        List<Integer> list = new LinkedList<>();
+//        for (int i = 0; i < num; i++) {
+//            list.add(i);
+//        }
+//    }
+//
+//}
