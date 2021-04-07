5d6da48e4f4e7e97a06eabefd973cae7aaced5c3
liuheyong <17682347237@163.com>
2021-04-07 16:22:54 +0800

    first commit

---
+++ src/main/java/com/agent/boot/lhy/SpringBootAgentApplication.java
@@ -0,0 +1,13 @@
+package com.agent.boot.lhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAgentApplication.class, args);
+    }
+
+}
